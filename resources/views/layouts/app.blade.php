82df37b43fb7d2b0c6917727db013f5f047c4bcf
SahinU88 <sahin@flyingfalcon.io>
2020-03-15 22:19:23 +0100

    feat(ui): add logo and favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,13 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
         <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon/apple-touch-icon.png') }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon/favicon-32x32.png') }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon/favicon-16x16.png') }}">
+        <link rel="manifest" href="{{ asset('favicon/site.webmanifest') }}">
+        <link rel="mask-icon" href="{{ asset('favicon/safari-pinned-tab.svg') }}" color="#5bbad5">
+        <meta name="msapplication-TileColor" content="#09364f">
+        <meta name="theme-color" content="#09364f">
     </head>
     <body class="flex flex-col min-h-screen antialiased leading-none">
 
